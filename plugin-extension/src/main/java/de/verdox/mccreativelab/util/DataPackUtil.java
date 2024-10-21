373e2ba85e1e777a8811165b9b271f940109e41d
verdox <mail.ysp@web.de>
2024-10-21 10:29:15 +0000

    Add remove datapack function

--- plugin-extension/src/main/java/de/verdox/mccreativelab/util/DataPackUtil.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/util/DataPackUtil.java
@@ -2,12 +2,19 @@ package de.verdox.mccreativelab.util;
 
 import de.verdox.mccreativelab.generator.Asset;
 import de.verdox.mccreativelab.util.io.ZipUtil;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class DataPackUtil {
     public static File installDataPack(String levelName, String dataPackName, Asset<?> dataPackAsset) {
         return ZipUtil.extractFilesFromZipFileResource(dataPackAsset.assetInputStream(), String.valueOf(Path.of(levelName + "/datapacks/" + dataPackName)));
+    }
+
+    public static void removeInstalledDataPack(String levelName, String dataPackName, Asset<?> dataPackAsset) throws IOException {
+        File dataPackDirectory = Path.of(levelName + "/datapacks/" + dataPackName).toFile();
+        FileUtils.deleteDirectory(dataPackDirectory);
     }
 }
