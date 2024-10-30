0992a97e6fbfd29a56ecbc099c62323bdf0d5abe
verdox <mail.ysp@web.de>
2024-10-30 19:09:14 +0000

    Removing mcc-serializer module

--- mcc-serializer/src/main/java/de/verdox/mccreativelab/serialization/generic/Serializer.java
+++
@@ -1,8 +0,0 @@
-package de.verdox.mccreativelab.serialization.generic;
-
-/**
- * @param <T> the data type that is serialized
- * @param <R> the generic SerializationContext
- */
-public interface Serializer<T, R extends SerializationElement> {
-}
