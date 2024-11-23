5e74a5eb87652ae47c09e0f516fdb89677366d4b
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-11-23 21:32:08 +0100

    fix: Biome Conversion Test

--- mcc-wrapper/src/test/java/de/verdox/mccreativelab/conversion/BiomeConversionTests.java
+++ mcc-wrapper/src/test/java/de/verdox/mccreativelab/conversion/BiomeConversionTests.java
@@ -3,16 +3,40 @@ package de.verdox.mccreativelab.conversion;
 import de.verdox.mccreativelab.TestBase;
 import de.verdox.mccreativelab.wrapper.platform.MCCPlatform;
 import de.verdox.mccreativelab.wrapper.world.level.biome.MCCBiome;
-import org.bukkit.block.Biome;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BiomeConversionTests extends TestBase {
 
     @Test
+    @DisplayName("Biome (native to MCC) Conversion Test")
     public void testBiomeConversion() {
-        Biome minecraftBiome = Biome.BADLANDS;
-        MCCBiome mccBiome = MCCPlatform.getInstance().getConversionService().wrap(minecraftBiome, MCCBiome.class);
+        Biome mcBiome = new Biome.BiomeBuilder()
+            .hasPrecipitation(true)
+            .temperature(24F)
+            .temperatureAdjustment(Biome.TemperatureModifier.FROZEN)
+            .downfall(3)
+            .specialEffects(new BiomeSpecialEffects.Builder()
+                .fogColor(21)
+                .waterColor(21)
+                .waterFogColor(21)
+                .skyColor(21)
+                .build())
+            .mobSpawnSettings(MobSpawnSettings.EMPTY)
+            .generationSettings(BiomeGenerationSettings.EMPTY)
+            .build();
+        MCCBiome mccBiome = MCCPlatform.getInstance().getConversionService().wrap(mcBiome, MCCBiome.class);
 
-        System.out.println(mccBiome.getBaseTemperature());
+        // assertion (eq)
+        assert mccBiome.hasPrecipitation() == mcBiome.hasPrecipitation();
+        assert mccBiome.getBaseTemperature() == mcBiome.getBaseTemperature();
+        assert mccBiome.getSpecialEffects().getFogColor() == mcBiome.getSpecialEffects().getFogColor();
+        assert mccBiome.getSpecialEffects().getWaterColor() == mcBiome.getSpecialEffects().getWaterColor();
+        assert mccBiome.getSpecialEffects().getWaterFogColor() == mcBiome.getSpecialEffects().getWaterFogColor();
+        assert mccBiome.getSpecialEffects().getSkyColor() == mcBiome.getSpecialEffects().getSkyColor();
     }
 }
