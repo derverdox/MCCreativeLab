de43c05770141d2ba31a1b8b17f4cc83be1582b8
verdox <mail.ysp@web.de>
2024-12-19 13:11:49 +0000

    Adjusting publishing

--- mcc-util/build.gradle.kts
+++ mcc-util/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("io.papermc.paperweight.userdev") version "1.7.3"
-    id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
+    id("xyz.jpenilla.run-paper") version "2.3.1" apply false // Adds runServer and runMojangMappedServer tasks for testing
 }
 
 group = "de.verdox.mccreativelab"
@@ -19,13 +19,29 @@ dependencies {
 }
 
 publishing {
-    publications.create<MavenPublication>("maven").from(components["java"]);
     publications {
-        create<MavenPublication>("lib") {
-            artifact(tasks.jar)
+        create<MavenPublication>("maven") {
+            pom {
+                groupId = "de.verdox.mccreativelab"
+                artifactId = "mcc-util"
+                version = providers.gradleProperty("version").get()
+                from(components["java"])
+                licenses {
+                    license {
+                        name = "GNU GENERAL PUBLIC LICENSE Version 3"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "verdox"
+                        name = "Lukas Jonsson"
+                        email = "mail.ysp@web.de"
+                    }
+                }
+            }
         }
     }
-    repositories.maven(repositories.mavenLocal())
 }
 
 tasks.test {
